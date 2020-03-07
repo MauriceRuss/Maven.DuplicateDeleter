da621e9cdc7a33dbde3a402882a49fc15d6824b3
Mo Russ <mauriceruss302@gmail.com>
2020-03-07 17:55:21 -0500

    IntegerDuplicateDeleterTests passed

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,59 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        
+        Integer[] intLeftArr = new Integer[0];
+        Integer[] arr;
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for(int j = 0;j< array.length;j++){
+                if(array[i] == array[j]){
+                    count++;
+                }
+            }
+            if(count < maxNumberOfDuplications){
+                arr = Arrays.copyOf(intLeftArr,intLeftArr.length+1);
+                arr[arr.length-1]=array[i];
+                intLeftArr = arr;
+            }
+            count=0;
+        }
+        return intLeftArr;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+
+        Integer[] remDupExArr = new Integer[0];
+        Integer[] arr;
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for(int j = 0;j<array.length;j++){
+                if(array[i]==array[j]){
+                    count++;
+                }
+            }
+            if(count != exactNumberOfDuplications){
+                arr = Arrays.copyOf(remDupExArr,remDupExArr.length+1);
+                arr[arr.length-1]=array[i];
+                System.out.println(arr[arr.length-1]);
+                remDupExArr = arr;
+            }
+            count=0;
+        }
+        return remDupExArr;
+    }
 }
